f8c59ae972b615e224e9d234b07c8bcfec02d9f3
Alexis Lemaire <alexis.lemaire2021@campus-eni.fr>
2021-09-28 19:56:52 +0200

    fix: delete route

--- router.js
+++ router.js
@@ -4,6 +4,7 @@ const r = express.Router();
 const CRUD = require('./m_crud/projects');
 r.post('/projects', CRUD.Insert);
 r.put('/projects/:id', CRUD.Update);
+r.delete('/projects/:id', CRUD.Delete);
 r.get('/projects/:id', CRUD.Select);
 r.get('/projects', CRUD.SelectAll);
 
