401b225206bf66444bd9a4d430d4c7e83be05305
Alexis Lemaire <alexis.lemaire2021@campus-eni.fr>
2021-09-28 14:12:10 +0200

    build: setup mySQL database connection

--- m_crud/projects.js
+++ m_crud/projects.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql');
-const db = mysql.createConnection({host: "localhost", user: "root", password: "", database: "mesProjets"});
+const db = mysql.createConnection({host: "sql4.freemysqlhosting.net", user: "sql4440706", password: "S7iFmztrFS", database: "sql4440706"});
 db.connect((err) => { if(err){ console.log(err); } });
 
 exports.Insert = (req,res) => { 
@@ -11,7 +11,7 @@ exports.Insert = (req,res) => {
         else 
         {
             const body = req.body;
-            db.query("INSERT INTO projects VALUES(?,?,?,?,?)", [body.title, body.description, body.date, body.link, body.github], (err) => {
+            db.query("INSERT INTO mesProjets VALUES(?,?,?,?,?)", [body.title, body.description, body.date, body.link, body.github], (err) => {
                 if(err !== null){
                     res.json({error: err.message});
                 } else {
@@ -32,7 +32,7 @@ exports.Update = (req,res) => {
         {
             const body = req.body;
             db.query(
-                "UPDATE projects SET title = ?, description = ?, date = ?, link = ?, github = ? WHERE id = ?", 
+                "UPDATE mesProjets SET title = ?, description = ?, date = ?, link = ?, github = ? WHERE id = ?", 
                 [body.title, body.description, body.date, body.link, body.github, req.params.id], 
                 (err) => {
                     if(err !== null){
@@ -46,21 +46,14 @@ exports.Update = (req,res) => {
 }
 
 exports.Select = (req,res) => {
-    db.query("SELECT * FROM projects WHERE id = ?", [req.params.id], (err, result) => {
+    db.query("SELECT * FROM mesProjets WHERE id = ?", [req.params.id], (err, result) => {
         res.json(result);
     });
 }
 
 exports.SelectAll = (req,res) => {
-    db.query("SELECT * FROM projects", (err, result) => {
-        if(result == false)
-        {
-            res.json({error: "La liste des projets n'a pas pu être récupérée"});
-        } 
-        else 
-        {
-            res.json(result);
-        }
+    db.query("SELECT * FROM mesProjets", (err, result) => {
+         res.json(result);
     });
 }
 
