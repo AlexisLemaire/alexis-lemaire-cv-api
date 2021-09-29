0c5fcc9c281d412924786c6e1c38810c197ac42d
Alexis Lemaire <alexis.lemaire2021@campus-eni.fr>
2021-09-29 16:27:09 +0200

    perf: migration from ExpressApp to FastifyApp

--- index.js
+++ index.js
@@ -1,9 +1,95 @@
-const cors = require('cors');
-const express = require('express');
-const app = express();
-const router = require('./router');
+const fastify = require("fastify");
+const app = fastify.fastify();
+require('dotenv').config();
 
-app.use(cors());
-app.use(express.json()); 
-app.use('/', router); 
+const mysql = require('mysql');
+const db = mysql.createConnection({host: process.env.host, user: process.env.user, password: process.env.password, database: process.env.database});
+db.connect((err) => { if(err){ console.log(err); } });
+
+// ********************************* ROUTES ************************************************** //
+
+// SELECT ALL
+app.get('/projects', async (req, rep) => {
+    db.query("SELECT * FROM mesProjets", (err, result) => {
+        rep.send(result);
+    });
+});
+
+// SELECT ONE 
+app.get('/projects/:id', async (req, rep) => {
+    db.query("SELECT * FROM mesProjets WHERE id = ?", [req.params.id], (err, result) => {
+        rep.send(result);
+    });
+});
+
+// CREATE ONE 
+app.post('/projects', async (req, rep) => {
+    db.query("SELECT secretKey FROM myKeys WHERE secretKey = ?", [req.body.secretKey], (err, result) => {
+        if(result == false)
+        {
+            res.json({error: "La secret key est incorrecte, donc l'ajout n'a pas eu lieu."});
+        } 
+        else 
+        {
+            const body = req.body;
+            db.query(
+                "INSERT INTO mesProjets(title,description,date,link,github) VALUES(?,?,?,?,?)", 
+                [body.title, body.description, body.date, body.link, body.github], 
+                (err) => {
+                    if(err !== null){
+                        res.json({error: err.message});
+                    } else {
+                        res.json({success: "L'ajout du projet s'est bien déroulé"});
+                    }
+                }
+            );
+        }
+    });
+});
+
+// DELETE ONE 
+app.delete('/projects/:id/:secretKey', async (req, rep) => {
+    db.query("SELECT secretKey FROM myKeys WHERE secretKey = ?", [req.params.secretKey], (err, result) => {
+        if(result == false)
+        {
+            res.json({error: "La secret key est incorrecte, donc la suppression n'a pas eu lieu"});
+        } 
+        else 
+        {
+            db.query("DELETE FROM mesProjets WHERE id = ?", [req.params.id], (err) => {
+                if(err !== null){
+                    res.json({error: err.message});
+                } else {
+                    res.json({success: "La suppression du projet s'est bien déroulée"});
+                }
+            });
+        }
+    });
+});
+
+// UPDATE ONE 
+app.put('/projects/:id', async (req, rep) => {
+    db.query("SELECT secretKey FROM myKeys WHERE secretKey = ?", [req.body.secretKey], (err, result) => {
+        if(result == false)
+        {
+            res.json({error: "La secret key est incorrecte, donc la mise à jour n'a pas eu lieu."});
+        } 
+        else 
+        {
+            const body = req.body;
+            db.query(
+                "UPDATE mesProjets SET title = ?, description = ?, date = ?, link = ?, github = ? WHERE id = ?", 
+                [body.title, body.description, body.date, body.link, body.github, req.params.id], 
+                (err) => {
+                    if(err !== null){
+                        res.json({error: err.message});
+                    } else {
+                        res.json({success: "La modification du projet s'est bien déroulée"});
+                    }
+                }
+            );
+        }
+    });
+});
+
 app.listen(process.env.PORT || 3001);
