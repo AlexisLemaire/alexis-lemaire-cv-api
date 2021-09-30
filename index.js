fe73ef06bbaae1f6e386b8b16497a983925b8b62
Alexis Lemaire <alexis.lemaire2021@campus-eni.fr>
2021-09-30 09:17:30 +0200

    fix: cors policy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fastify = require("fastify");
 const app = fastify.fastify();
 require('dotenv').config();
 
-app.register(require('fastify-cors'), { origin: true });
+app.register(require('fastify-cors'), { origin: '*' });
 
 const mysql = require('mysql');
 const db = mysql.createConnection({host: process.env.host, user: process.env.user, password: process.env.password, database: process.env.database});
