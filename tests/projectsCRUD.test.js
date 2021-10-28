ed7d3e12e25df6ae91c05f7aa18a0aba0f9754ea
Alexis Lemaire <alexis.lemaire2021@campus-eni.fr>
2021-10-28 17:00:41 +0200

    fix: add Client field to projectsCRUD

--- tests/projectsCRUD.test.js
+++ tests/projectsCRUD.test.js
@@ -5,23 +5,22 @@ describe('UNIT TESTS PROJECTS CRUD', () => {
     it('should POST a project & verify it was correctly POST', async () => {
         let res = (await axios.post("http://alexis-lemaire-cv-api.herokuapp.com/projects", fakeProject)).data;
         await (fakeProject.id = res.insertId);
-        await console.log(res);
-        await expect(res).toHaveProperty("success");
+        expect(res).toHaveProperty("success");
     });
 
     it('should SELECT the fakeProject', async () => {
         let res = (await axios.get(`https://alexis-lemaire-cv-api.herokuapp.com/projects/${fakeProject.id}`)).data;
-        await expect(res).toBeDefined();
+        expect(res).toBeDefined();
     });
 
     it('should UPDATE the fakeProject & verify it was correctly UPDATE', async () => {
         await (fakeProject.title = "Mon Faux Projet avec titre changé");
         let res = (await axios.put(`https://alexis-lemaire-cv-api.herokuapp.com/projects/${fakeProject.id}`, fakeProject)).data;
-        await expect(res).toHaveProperty("success");
+        expect(res).toHaveProperty("success");
     });
 
     it('should DELETE the fakeProject & verify it was correctly DELETE', async () => {
         let res = (await axios.delete(`https://alexis-lemaire-cv-api.herokuapp.com/projects/${fakeProject.id}/${fakeProject.secretKey}`)).data;
-        await expect(res).toHaveProperty("success");
+        expect(res).toHaveProperty("success");
     });
 });
